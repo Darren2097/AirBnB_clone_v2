243ef69d7dbf0113e75208a7dcfd44054046fe22
Darren <darren2097@gmail.com>
2022-12-06 17:00:28 +0200

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,5 +66,3 @@ class FileStorage:
             key = '{}.{}'.format(type(obj).__name__, obj.id)
             if key in self.__objects:
                 del self.__objects[key]
-        else:
-            return
