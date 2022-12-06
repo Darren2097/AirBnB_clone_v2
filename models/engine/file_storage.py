c9f6689b6678135f2245c03c5f0dc4e08932e8ff
Darren <darren2097@gmail.com>
2022-12-06 17:03:26 +0200

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,7 +62,8 @@ class FileStorage:
         """delete obj from __objects if it’s inside -
         if obj is equal to None, the method should not do anything"""
 
-        if obj is not None:
-            key = '{}.{}'.format(type(obj).__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
+        if not obj:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.__objects:
+            del self.__objects[key]
