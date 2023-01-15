d7ffcd26bc15e5a01aefcaffd8377ed755db7026
Darren <darren2097@gmail.com>
2023-01-15 13:34:20 +0200

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return Hello HBNB"""
+    
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return HBNB"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """returns C message"""
+
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
